4b90af233c6e98c0b8dc36eccd8f428250309f87
rgdane <regadanew1@gmail.com>
2024-09-06 10:38:10 +0700

    Pertemuan 2 - Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'NIM : 2241760113 <br> Nama : Rega Dane Wijayanta';
+    }
+}
