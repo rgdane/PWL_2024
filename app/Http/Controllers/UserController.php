9ef9c7a7c28266e33380416dbd5507c57d280a3c
rgdane <regadanew1@gmail.com>
2024-09-16 08:31:18 +0700

    Pertemuan 3 - Praktikum 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function user($id, $name){
-        return view('pos.user')
-        ->with('id', $id)
-        ->with('name', $name);
+    public function index(){
+        $data = [
+        'nama' => 'Pelanggan Pertama'
+        ];
+
+        UserModel :: where('username','customer-1')->update($data); 
+        
+        $user = UserModel :: all(); // ambil semua data dari tabel m_user
+        return view('pos.user', ['data' => $user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,64 +15,6 @@ use App\Http\Controllers\UserController;
 
 Route::get('/level', [LevelController::class,'index']);
 Route::get('/kategori', [KategoriController::class,'index']);
-
-Route::get('/greeting', [WelcomeController::class,'greeting']);
+Route::get('/user', [UserController::class,'index']);
 
 Route::get('/', [HomeController::class,'index']);
-
-Route::get('/user/{id}/name/{name}', [UserController::class,'user']);
-
-Route::get('/category/food-beverage', [ProductController::class,'foodBeverage']);
-Route::get('/category/beauty-health', [ProductController::class,'beautyHealth']);
-Route::get('/category/home-care', [ProductController::class,'homeCare']);
-Route::get('/category/baby-kid', [ProductController::class,'babyKid']);
-
-Route::get('/transaction', [POSController::class,'transaction']);
-
-// Route::get('/greeting', function () {
-//     return view('blog.hello', ['name' => 'Dane']);
-//     });
-
-Route::resource('photos', PhotoController::class);
-
-Route::resource('photos', PhotoController::class)->only([
-    'index', 'show'
-]);
-
-Route::resource('photos', PhotoController::class)->except([
-    'create', 'store', 'update', 'destroy'
-]);
-
-
-Route::get('/hello', [WelcomeController::class, 'hello']);
-
-Route::get('/world', function () {
-    return 'World';
-});
-
-Route::get('/about', [AboutController::class, 'about']);
-
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
-
-// Route::get('/about', function () {
-//     return 'NIM : 2241760113 <br> Nama : Rega Dane Wijayanta';
-// });
-
-Route::get('/user/{name}', function ($name) {
-    return 'Nama saya '.$name;
-});
-
-Route::get('/posts/{post}/comments/{comment}', function
-($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-
-// Route::get('/articles/{id}', function
-// ($id) {
-//     return 'Halaman Artikel dengan ID '.$id;
-// });
-
-Route::get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-});
-    
