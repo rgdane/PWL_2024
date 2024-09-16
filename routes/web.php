4f6e2c653dc535b329b290e9599b803ad29212c2
rgdane <regadanew1@gmail.com>
2024-09-16 07:57:46 +0700

    Pertemuan 3 - Praktkum 5

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
@@ -13,6 +14,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 
 Route::get('/level', [LevelController::class,'index']);
+Route::get('/kategori', [KategoriController::class,'index']);
 
 Route::get('/greeting', [WelcomeController::class,'greeting']);
 
