8e12f1e2792873702907066e7037edbf47a7d928
rgdane <regadanew1@gmail.com>
2024-09-20 11:07:27 +0700

    Pertemuan 4 - Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,13 +11,21 @@ class UserController extends Controller
     public function index(){
         $user = UserModel :: firstOrNew(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ]
         );
+        $user->username = 'manager12';
+
         $user->save();
+
+        $user->wasChanged (); // true
+        $user->wasChanged ('username'); // true
+        $user->wasChanged( ['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged( ['nama', 'username'])); // true
 
         return view('pos.user', ['data' => $user]);
     }
