d000e5902c22f913858acfe6e95e68afc5db8d8c
rgdane <regadanew1@gmail.com>
2024-09-20 10:43:23 +0700

    Pertemuan 4 - Praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,16 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
+        $user = UserModel :: firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+        $user->save();
 
-        $user = UserModel :: where('level_id', 2)->count();
-        // dd($user);
         return view('pos.user', ['data' => $user]);
     }
 }
