b30c48b4c1186542b272eaf65ef8ae10a7390c41
rgdane <regadanew1@gmail.com>
2024-09-22 15:52:16 +0700

    Pertemuan 4 - Praktikum 2.6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,24 +9,48 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel :: firstOrNew(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
-        $user->username = 'manager12';
+        $user = UserModel :: all();
+
+        return view('pos.user', ['data' => $user]);
+    }
+    
+    public function tambah(){
+        return view('pos.user_tambah');
+    }
+
+    public function tambahSimpan(Request $request){
+        UserModel :: create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash :: make('$request->password'),
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+    }
+
+    public function ubah($id){
+        $user = UserModel :: find($id);
+
+        return view('pos.user_ubah', ['data' => $user]);
+    }
+
+    public function ubahSimpan($id, Request $request){
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
 
         $user->save();
 
-        $user->wasChanged (); // true
-        $user->wasChanged ('username'); // true
-        $user->wasChanged( ['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        dd($user->wasChanged( ['nama', 'username'])); // true
+        return redirect('/user');
+    }
+    
+    public function hapus($id){
+        $user = UserModel::find($id);
+        $user->delete();
 
-        return view('pos.user', ['data' => $user]);
+        return redirect('/user');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ use App\Http\Controllers\UserController;
 Route::get('/level', [LevelController::class,'index']);
 Route::get('/kategori', [KategoriController::class,'index']);
 Route::get('/user', [UserController::class,'index']);
+Route::get('/user/tambah', [UserController::class,'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class,'tambahSimpan']);
+Route::get('/user/ubah/{id}', [UserController::class,'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class,'ubahSimpan']);
+Route::get('/user/hapus/{id}', [UserController::class,'hapus']);
 
 Route::get('/', [HomeController::class,'index']);
 
