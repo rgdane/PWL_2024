4e95acf1ba45271463116aab3aac156e692b6d93
rgdane <regadanew1@gmail.com>
2024-10-20 16:38:09 +0700

    Pertemuan 8 - Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -136,6 +136,7 @@ Route::middleware( ['auth'])->group(function(){
             
             Route::get('/import', [BarangController::class, 'import']); //ajax form upload excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); //ajax import excel
+            Route::get('/export_excel', [BarangController::class, 'export_excel']); //export excel
         });
     });
 
